6d7cbcc44e773753267745537609b0e672436ecc
brookelove <brookelove@Brookes-iMac-2.local>
2022-11-16 21:18:56 -0800

    starting information on different aspects

--- find_upper_lower_num_punc.py
+++ find_upper_lower_num_punc.py
@@ -3,7 +3,30 @@
 2. use split() methond
     a. append it to a new list
 3. use a for loop to go through the seperated_sentence_list
-    a. have 4 different lits all global (uppercase, lowercase, digits, punctuation)
+    a. have 4 different lists all global (uppercase, lowercase, digits, punctuation)
         - using import string (string.ascii_lowercase,string.ascii_uppercase, string.ascii_punctuation, string.ascii_lowercase, string.digits)
 4. print results in a table that are formated well using docstring and f-types
 '''
+import re
+import string
+docstring = '''I love to code with python. I love having three dogs with me. I want to learn how to make bread.'''
+upper = 0
+lower = 0
+digits = 0
+punc = 0
+i = 0
+sentences = re.split(' . ', docstring)
+
+for i in sentences:
+    # print(i)  # returns each sentenece with.
+    # print(type(i))
+    placeholder_sentence = list(i)
+    # for j in i:
+    #     if j in string.ascii_lowercase:
+    #         lower += 1
+    #     if j in string.ascii_uppercase:
+    #         upper += 1
+    #     if j in string.digits:
+    #         digits += 1
+    #     if j in string.punctuation:
+    #         punc += 1
--- increment_digits.py
+++ increment_digits.py
@@ -8,3 +8,22 @@
     i. if compare_str == user_input; then print Number accepted!
     ii. else; call the user input and then run the function again 
 '''
+
+
+def check_increment():
+
+    while True:
+        user_input = input("Please enter a 3-digit: ")
+        placeholder = list(user_input)
+        # print(placeholder)  # prints ['4', '5', '6']
+
+        if '.' in str(user_input):
+            print("---> Error: This is not an integer. Please re-enter.")
+        elif len(placeholder) > 3:
+            print("---> Error: You did not enter a 3-digit number")
+        else:
+            print("Number Accepted!")
+            break
+
+
+check_increment()
