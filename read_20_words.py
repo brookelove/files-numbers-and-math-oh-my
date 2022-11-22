d7049242ae69f4f4965b246df23e6454008a8244
brookelove <brookelove@Brookes-iMac-2.local>
2022-11-21 23:26:36 -0800

    creating a list of words with the max of 20 words

--- read_20_words.py
+++ read_20_words.py
@@ -20,16 +20,88 @@ class CannotOpenFile(Exception):
     pass
 
 
+class OutOfBounds(Exception):
+    pass
+
+
 READFILE = "cs412_3_4_input.txt"
 MAX_WORDDS = 20
-MAX_WORDS_PER_LINE = 5
-WRITE_FILE = "cs412_3_4_output.txt"
+WORDS_PER_LST = 5
+WRITEFILE = "cs412_3_4_output.txt"
+sectioned_words = []
+sectioned_lines = []
 # 1. open file using a try-catch block
-try:
-    text_file = open(READFILE, "r")
-except CannotOpenFile:
-    print(f"--> Error: Cannot open file {READFILE}")
+
+
+def writing_files(lst):
+    try:
+        with open(WRITEFILE, 'w') as text_file:
+            text_file.write(lst)
+        print(f"Success! Output written to: {WRITEFILE}")
+        text_file.close()
+    except CannotOpenFile:
+        print("error")
+
+
+# def create_five_lst(lst):
+#     i = 0
+#     for i in len(lst):
+#         line = ' '.join(lst[i])
+#         sectioned_lines.append(line)
+
+
+def create_twenty_lst(text_file):
+    all_data = text_file.readline()
+    # text_file.close()
+    # print(all_data) # list of all data in one line
+    # print(type(all_data)) # returngs a string
+    # print(len(all_data))  # gives you the lenght of the amount in the data, returns 1
+    # 3. convert data to a list of all of the words, remove
+    lst_of_words = all_data.split(' ')
+    # print(lst_of_words) # returns all of the list
+    # print(type(lst_of_words)) # returns a list
+    # print(len(lst_of_words)) # should return 20
+    # 4. confirm that the sentence is 20 words
+    if len(lst_of_words) > MAX_WORDDS:
+        print('--> Error: This sentence does not contain 20 words')
+        sys.exit()
+
+# for loop to seperate the 5 words
+    for i in range(0, MAX_WORDDS, WORDS_PER_LST):
+        n = i
+        placeholder = lst_of_words[n:n + WORDS_PER_LST]
+        sectioned_words.append(placeholder)
+    i = 0
+    for i in range(len(sectioned_words)):
+        line = ' '.join(sectioned_words[i])
+        line = line + '\n'
+        sectioned_lines.append(line)
+        i += 1
+        # print(len(sectioned_lines))
+        # writing_files(sectioned_lines)
+    # for index in range(len(sectioned_lines)):
+    #     writing_files(sectioned_lines[index])
+    #     index += 1
+    conjoined_line = ''.join(sectioned_lines)
+    print(type(conjoined_line))
+    writing_files(conjoined_line)
+
+
+def reading_files():
+    try:
+        text_file = open(READFILE, "r")
+        create_twenty_lst(text_file)
+        text_file.close()
+    except CannotOpenFile:
+        print(f"--> Error: Cannot open file {READFILE}")
 # 2. Make it readable
-all_data = text_file.readlines()
-print(all_data)
-# 3. convert data to a list of all of the words, remove
+
+
+reading_files()
+
+# print(lst_of_words[n:n + WORDS_PER_LST]) # gives a seperated list of words
+# # print(grouped)
+# print(sectioned_words)
+# remove_new_line = data[:-1]
+# lst_of_words.append(remove_new_line)
+# print(lst_of_words)
